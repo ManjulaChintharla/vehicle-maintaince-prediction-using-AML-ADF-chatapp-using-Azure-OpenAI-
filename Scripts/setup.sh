48c5474e17f484c7af46b4f5e0a2f7e56496c237
Manjula Chintharla <84832942+ManjulaChintharla@users.noreply.github.com>
2025-03-21 20:30:29 +0530

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -43,11 +43,11 @@ az configure --defaults workspace=$WORKSPACE_NAME
 
 # Create compute instance
 echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
-az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS11_V2 --type ComputeInstance 
+az ml compute create --name ${COMPUTE_INSTANCE} --size Standard_DS3_v2 --type ComputeInstance 
 
 # Create compute cluster
 echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
+az ml compute create --name ${COMPUTE_CLUSTER} --size Standard_DS3_v2 --max-instances 2 --type AmlCompute 
 
 # Create Azure data factory
 echo "Creating a Azure data factory with name: " $ADF_NAME
