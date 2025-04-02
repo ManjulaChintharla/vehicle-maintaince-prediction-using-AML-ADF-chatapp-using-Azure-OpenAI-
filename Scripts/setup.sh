832dbf1d55cfeef540f7583ba5fc1b72672237d6
Manjula Chintharla <84832942+ManjulaChintharla@users.noreply.github.com>
2025-04-02 11:31:26 +0530

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -137,6 +137,7 @@ az role assignment create --assignee $(az account show --query user.name --outpu
 echo "Creating an Azure database for PostgreSQL with name: $Azure_POSTGRESQL_NAME"
 
 az postgres flexible-server create --location westus --resource-group $RESOURCE_GROUP --name $Azure_POSTGRESQL_NAME --admin-user $USERNAME --admin-password $PASSWORD --sku-name Standard_D2s_v3 --tier GeneralPurpose --storage-size 128 --tags "Environment=Dev" --version 14 --high-availability Disabled --public-access Enabled
+az postgres flexible-server firewall-rule create --resource-group $RESOURCE_GROUP --name $Azure_POSTGRESQL_NAME --rule-name "AllowAllAzureServices"  --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 
 
 echo "Username of postgresql is  " : $USERNAME
